d1fce261aa9e6bc0beab840090bc247a99ecd246
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-08-30 23:49:35 +0300

    typing comments and organize the code

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,49 +1,60 @@
 
 'use strict';
+
+// Create a random generator
 let dice = Math.trunc(Math.random() * 20 + 1);
-let winner = true;
-console.log(dice);
-let guess;
+
+
+
+//prepare your selectors 🚀
 let highScore = document.querySelector('.highscore');
 let score = document.querySelector('.score');
 const initialScore = Number(score.textContent);
 let stat = document.querySelector('.number-stat');
+let winner = false;
+
+// action on clicking check button 🥳
 document.querySelector('.check').addEventListener("click", () => {
-    if (Number(score.textContent) === 0) {
-        stat.textContent = ` You've lost😠`
-        document.querySelector('body').style.background = 'linear-gradient(90deg, rgba(102,6,27,1) 0%, rgba(200,5,5,1) 100%)';
-        document.querySelector('.guess').style.background = 'linear-gradient(90deg, rgba(102,6,27,1) 0%, rgba(200,5,5,1) 100%)';
-    } else {
+    // If you score is 0
+    if (!winner) {
+        if (Number(score.textContent) === 0) {
+            stat.textContent = ` You've lost😠`
+            document.querySelector('body').style.background = 'linear-gradient(90deg, rgba(102,6,27,1) 0%, rgba(200,5,5,1) 100%)';
+            document.querySelector('.guess').style.background = 'linear-gradient(90deg, rgba(102,6,27,1) 0%, rgba(200,5,5,1) 100%)';
+        } else {
+            let guess = document.querySelector('.guess').value;
 
 
+            // if empty guess
+            if (!guess && guess != '0') {
+                stat.textContent = ` ❌ NO number `;
+            } else if (Number(guess) === Number(dice)) {
+                stat.textContent = `🥳 You've Won`;
+                highScore.textContent = Math.max(Number(score.textContent), Number(highScore.textContent));
+                document.querySelector('.winning>span').textContent = guess;
+                document.querySelector('body').style.background = 'linear-gradient(90deg, rgba(6,102,12,1) 0%, rgba(41,200,5,1) 100%)';
+                document.querySelector('.guess').style.background = 'linear-gradient(90deg, rgba(6,102,12,1) 0%, rgba(41,200,5,1) 100%)';
+            }
 
 
-
-        guess = document.querySelector('.guess').value;
-        if (!guess && guess != '0') {
-            stat.textContent = ` ❌ NO number `;
-        } else if (Number(guess) === Number(dice)) {
-            stat.textContent = `🥳 You've Won`;
-            highScore.textContent = Math.max(Number(score.textContent), Number(highScore.textContent));
-            document.querySelector('.winning>span').textContent = guess;
-            document.querySelector('body').style.background = 'linear-gradient(90deg, rgba(6,102,12,1) 0%, rgba(41,200,5,1) 100%)';
-            document.querySelector('.guess').style.background = 'linear-gradient(90deg, rgba(6,102,12,1) 0%, rgba(41,200,5,1) 100%)';
-        }
-
-
-        else {
-            if (Number(guess) > Number(dice))
-                stat.textContent = ` 📈 Too High`;
-            else if (Number(guess) < Number(dice))
-                stat.textContent = ` 📉 Too low`;
-            score.textContent = Number(score.textContent) - 1;
+            else {
+                if (Number(guess) > Number(dice))
+                    stat.textContent = ` 📈 Too High`;
+                else if (Number(guess) < Number(dice))
+                    stat.textContent = ` 📉 Too low`;
+                score.textContent = Number(score.textContent) - 1;
+            }
         }
     }
 
 });
 
 
+// reseting the page without reloading by clicking the again button
 document.querySelector('.again').addEventListener('click', function () {
+    // change the dice
+    dice = Math.trunc(Math.random() * 20 + 1);
+
     score.textContent = initialScore;
     document.querySelector('.winning>span').textContent = '?';
     stat.textContent = 'start guessing ...';
