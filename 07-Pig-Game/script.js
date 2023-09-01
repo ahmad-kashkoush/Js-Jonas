a1e069bec50d63cca3373c243abf38320f500921
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-09-01 06:34:14 +0300

    Fixed the winner

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -10,16 +10,23 @@ curr[0] = document.querySelector('#current--0');
 curr[1] = document.querySelector('#current--1');
 const diceImg = document.querySelector('.dice');
 diceImg.classList.add('hidden');
-
+let GameIsFinished = false;
+const initialWinnerScore = 100;
 // functions
 const changePlayer = function (value, plr) {
 
     score[plr].textContent = Number(score[plr].textContent) + value;
     curr[plr].textContent = 0;
-
     let curPlayer = 1 - plr;
-    player[curPlayer].classList.add('player--active');
-    player[plr].classList.remove('player--active');
+    if (initialWinnerScore <= Number(score[plr].textContent)) {
+        GameIsFinished = true;
+        player[plr].classList.remove('player--active');
+        player[plr].classList.add('player--winner');
+
+    } else {
+        player[curPlayer].classList.add('player--active');
+        player[plr].classList.remove('player--active');
+    }
     return curPlayer;
 
 }
@@ -36,29 +43,37 @@ const rollDice = function (plr) {
         curr[plr].textContent = Number(curr[plr].textContent) + dice;
     }
     return plr;
+
 }
 
 
 // Events
 let curPlayer = 0;
 document.querySelector('.btn--hold').addEventListener('click', function () {
-    let plr = curPlayer;
-    let value = Number(curr[plr].textContent);
-    value = value == 1 ? 0 : value;
-    curPlayer = changePlayer(value, plr);
+    if (!GameIsFinished) {
+        let plr = curPlayer;
+        let value = Number(curr[plr].textContent);
+        value = value == 1 ? 0 : value;
+        curPlayer = changePlayer(value, plr);
+    }
 
 });
 
 document.querySelector('.btn--roll').addEventListener('click', () => {
-    curPlayer = rollDice(curPlayer);
+    if (!GameIsFinished) {
+        curPlayer = rollDice(curPlayer);
+    }
 });
 
 document.querySelector('.btn--new').addEventListener('click', function () {
     diceImg.classList.add('hidden');
+    GameIsFinished = false;
     for (let i = 0; i < curr.length; i++) {
         curr[i].textContent = 0;
         score[i].textContent = 0;
     }
     player[0].classList.add('player--active');
     player[1].classList.remove('player--active');
+    player[0].classList.remove('player--winner');
+    player[1].classList.remove('player--winner');
 });
