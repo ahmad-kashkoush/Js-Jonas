c42fe36720027ecec9d2d9b4919b724a71739963
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-09-03 19:18:06 +0300

    Update ReadMes

--- 09-Data-Structures-Operators/ReadMe.md
+++ 09-Data-Structures-Operators/ReadMe.md
@@ -77,3 +77,59 @@ const {
 } = parentObject;
 console.log(p1, p2);
 ```
+
+---
+
+## ... spread operator😶‍🌫️
+
+#### It works on all **Iterables**:
+
+    1. Strings
+    2. Arrays
+    3. maps
+    4. Sets
+    5. doesn't work on objects
+
+#### Muliptle values seperated by comma only for :
+
+    1. function arguments
+    2. array assignments --> const a=...dflj;a
+
+#### Used to avoid array shallow copy
+
+```js
+const arr = [1, 2, 3];
+const arrCpy = arr;
+arrCpy[0] = 10; // I only need to change arrCpy[0] not arr[0]
+console.log(arr[0], arrCpy[0]); // arr[0] is changed
+
+// Solution⭐⭐⭐⭐⭐
+const deepCpy = [...arr];
+deepCpy[2] = 15;
+console.log(deepCpy[2], arr[2]); // only deepCpy is changing
+```
+
+#### Joining two arrays
+
+```js
+const mergedArrays = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(mergedArrays);
+```
+
+#### passing arguments to functions
+
+```js
+const arrIngredients = ['chicken', 'meat', 'cheese'];
+restaurant.orderPasta(...arrIngredients);
+```
+
+#### Deep Clone for objects and avoid shallow copy 🏆
+
+```js
+const newObj = {
+  p1: [1, 2, 3, 4, 5, [1, 2, 3]],
+};
+const newObjCpy = { ...newObj };
+newObjCpy.p1 = [50, 60];
+console.log(newObj.p1, newObjCpy.p1);
+```
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -11,7 +11,11 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+    orderPasta: function (ng1, ng2, ng3) {
+        console.log(
+            `Here is you delicus Pasta with Ingrediets ${ng1} , ${ng2}, and ${ng3}`
+        );
+    },
     openingHours: {
         thu: {
             open: 12,
@@ -27,9 +31,30 @@ const restaurant = {
         },
     },
 };
+// ! Spread operator
+//  Used to avoid array shallow copy
+const arr = [1, 2, 3];
+const arrCpy = arr;
+arrCpy[0] = 10;// I only need to change arrCpy[0] not arr[0]
+console.log(arr[0], arrCpy[0]);// arr[0] is changed 
+const deepCpy = [...arr];
+deepCpy[2] = 15;
+console.log(deepCpy[2], arr[2]);// only deepCpy is changing
 
+// Joining two arrays
+const mergedArrays = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(mergedArrays);
 
-
+// passing arguments to funcitons
+const arrIngredients = ['chicken', 'meat', 'cheese'];
+restaurant.orderPasta(...arrIngredients);
+// Deep Clone for objects and avoid shallow copy 🏆
+const newObj = {
+    p1: [1, 2, 3, 4, 5, [1, 2, 3]]
+};
+const newObjCpy = { ...newObj };
+newObjCpy.p1 = [50, 60];
+console.log(newObj.p1, newObjCpy.p1);
 // // ! Array destructing
 // const arr = [2, 3, 4];
 // const [a, b, c] = arr;
@@ -47,4 +72,3 @@ const restaurant = {
 // // You can set Default values before destructing
 // const [h = 1, m = 1, n = 1, t = 10] = arr;
 // console.log(h, m, n, t);
-
