34bbefb3c307f4b3f2a399f13ee019258060c231
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-09-09 23:45:57 +0300

    10-function section 🌒

--- 10-Functions/ReadMe.md
+++ 10-Functions/ReadMe.md
@@ -1,15 +1,3 @@
-<!-- TOC -->
-
-- [-Functions](#-functions)
-  - [Default Parameters ⭐](#default-parameters-)
-  - [Passing Reference vs value type 🧭](#passing-reference-vs-value-type-%F0%9F%A7%AD)
-  - [First Class vs Higher order function 📓](#first-class-vs-higher-order-function-) - [Just a theory video return to slides 🌲](#just-a-theory-video-return-to-slides-)
-  - [Accepting a callback function 🚀](#accepting-a-callback-function-)
-  - [Function return function 🙃](#function-return-function-)
-  - [Apply, call and **bind** Method 🛩️](#apply-call-and-bind-method-)
-
-<!-- /TOC -->
-
 # 10-Functions
 
 ## Default Parameters ⭐
@@ -147,4 +135,72 @@ document
 // why didn't use call, or apply
 // --> will call the function and I need callback
 // ⭐⭐⭐ use bind method to set this keyword and without calling the funciton
+```
+
+# Coding Challenge #1
+
+```js
+/*Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+   What is your favourite programming language?
+   0: JavaScript
+   1: Python
+   2: Rust
+   3: C++
+   (Write option number)
+
+   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+\*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer(str) {
+    const displayQuestion = [str, this.question, this.options.join('\n')].join(
+      '\n'
+    );
+    // console.log(displayQuestion);
+    const input = Number(prompt(displayQuestion));
+    this.checkAnswer(input);
+    this.answers[input]++;
+    this.displayResults('string');
+  },
+  checkAnswer(input) {
+    if (input >= 0 && input < this.answers.length) return input;
+    return this.registerNewAnswer('InValid Number');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') console.log(this.answers);
+    else console.log(`Poll results are ${this.answers.join(', ')}`);
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+const displayArr = poll.displayResults;
+
+displayArr.call({ answers: [5, 2, 3] });
+displayArr.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 ```
--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,9 +2,40 @@
 
 // // # 10-Functions
 
+//  ## Closures
+
+// A function has access to variables, in which 
+// it's execution context was created
+// Closure has higher priority than scope chain for the same variable
+
+// ## Coding Challenge 2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red'
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  })
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Coding Challenge #1
 
-/* 
+/*
 Let's build a simple poll app!
 
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
@@ -19,10 +50,10 @@ Here are your tasks:
         2: Rust
         3: C++
         (Write option number)
-  
+
   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
 HINT: Use many of the tools you learned about in this and the last section 😉
@@ -34,41 +65,41 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
-const poll = {
-    question: 'What is your favourite programming language?',
-    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-    // This generates [0, 0, 0, 0]. More in the next section 😃
-    answers: new Array(4).fill(0),
-    registerNewAnswer(str) {
-        const displayQuestion = [str, this.question, this.options.join('\n')].join('\n');
-        // console.log(displayQuestion);
-        const input = Number(prompt(displayQuestion));
-        this.checkAnswer(input);
-        this.answers[input]++;
-        this.displayResults('string');
-    },
-    checkAnswer(input) {
-        if (input >= 0 && input < this.answers.length)
-            return input;
-        return this.registerNewAnswer('InValid Number');
-    },
-    displayResults(type = 'array') {
-        if (type === 'array')
-            console.log(this.answers);
-        else console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
+// const poll = {
+//     question: 'What is your favourite programming language?',
+//     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//     // This generates [0, 0, 0, 0]. More in the next section 😃
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer(str) {
+//         const displayQuestion = [str, this.question, this.options.join('\n')].join('\n');
+//         // console.log(displayQuestion);
+//         const input = Number(prompt(displayQuestion));
+//         this.checkAnswer(input);
+//         this.answers[input]++;
+//         this.displayResults('string');
+//     },
+//     checkAnswer(input) {
+//         if (input >= 0 && input < this.answers.length)
+//             return input;
+//         return this.registerNewAnswer('InValid Number');
+//     },
+//     displayResults(type = 'array') {
+//         if (type === 'array')
+//             console.log(this.answers);
+//         else console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
 
-}
+// }
 
 
 
-document.querySelector('.poll')
-    .addEventListener('click', poll.registerNewAnswer.bind(poll))
+// document.querySelector('.poll')
+//     .addEventListener('click', poll.registerNewAnswer.bind(poll))
 
-const displayArr = poll.displayResults;
+// const displayArr = poll.displayResults;
 
-displayArr.call({ answers: [5, 2, 3] });
-displayArr.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+// displayArr.call({ answers: [5, 2, 3] });
+// displayArr.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 
 
 
