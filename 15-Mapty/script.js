bbcea52b33589a8696a5a982aa1a8c379022ca13
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-09-28 15:49:04 +0300

    messy version of form

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -12,14 +12,15 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Geolocation
-
+let map;
+let mapEvent;
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
 
 
         const coordinates = [position.coords.latitude, position.coords.longitude];
         // Getting the map From leaflet
-        var map = L.map('map').setView(coordinates, 13);
+        map = L.map('map').setView(coordinates, 13);
 
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -29,53 +30,71 @@ if (navigator.geolocation) {
             .bindPopup('A pretty CSS popup.<br> Easily customizable.')
             .openPopup();
         // to identify the click coords in the map
-        map.on('click', function (mapEvent) {
+        map.on('click', function (e) {
+            mapEvent = e;
             // My code to implement the form  features🏆
             form.classList.remove('hidden');
-
-            document.addEventListener('keydown', function (e) {
-                if (e.key != 'Enter') return;
-                e.preventDefault();
-                const inputTypeValue = inputType.value.toLowerCase();
-                const inputDistanceValue = inputDistance.value;
-                const inputDurationValue = inputDistance.value;
-                console.log(inputTypeValue, inputDistanceValue, inputDurationValue);
-
-                const { lat, lng } = mapEvent.latlng;
-                // console.log(lat, lng);
-                let className = 'cycling-popup';
-                const dateOptions = {
-                    month: "long",
-                    day: "numeric",
-                }
-                const today = new Intl.DateTimeFormat("en-US", dateOptions).format(Date.now())
-                let popUpContent = `🚴‍♀️ Cycling on ${today}`;
-                if (inputTypeValue == 'running') {
-                    className = 'running-popup';
-                    popUpContent = `🏃‍♂️ Running on ${today}`;
-                }
-                const markerOptions = {
-                    autoClose: false,
-                    maxWidth: 250,
-                    maxHeight: 100,
-                    closeOnClick: false,
-                    className: className
-
-                }
-                L
-                    .marker([lat, lng]).addTo(map)
-                    .bindPopup(L.popup(markerOptions)).setPopupContent(popUpContent)
-                    .openPopup();
-
-            })
+            inputDistance.focus();
 
 
 
-        });
 
-    },
-        function () {
-            console.log("The second funciton will be used if the first didn't apply");
-        }
-    )
+
+
+            // });
+
+        },
+            function () {
+                console.log("The second funciton will be used if the first didn't apply");
+            }
+        )
+    });
 }
+//  What I've Done is
+// 1. I've written spaggettie code by 
+// 2. I should've writen marker and map code at submit from event
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    inputDistance.value = inputDuration.value = inputCadence.value = '';
+    const inputTypeValue = inputType.value.toLowerCase();
+
+
+
+    // console.log(lat, lng);
+    let className = 'cycling-popup';
+    const dateOptions = {
+        month: "long",
+        day: "numeric",
+    }
+    const today = new Intl.DateTimeFormat("en-US", dateOptions).format(Date.now())
+    let popUpContent = `🚴‍♀️ Cycling on ${today}`;
+    if (inputTypeValue == 'running') {
+        className = 'running-popup';
+        popUpContent = `🏃‍♂️ Running on ${today}`;
+    }
+    // display Marker
+    const { lat, lng } = mapEvent.latlng;
+    const markerOptions = {
+        autoClose: false,
+        maxWidth: 250,
+        maxHeight: 100,
+        closeOnClick: false,
+        className: className
+
+    }
+    L
+        .marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup(markerOptions)).setPopupContent(popUpContent)
+        .openPopup();
+
+});
+
+inputType.addEventListener('change', function (e) {
+    if (inputType.value.toLowerCase() == 'cycling') {
+        inputElevation.closest('.form__row').classList.remove('form__row--hidden');
+        inputCadence.closest('.form__row').classList.add('form__row--hidden');
+    } else if (inputType.value.toLowerCase() == 'running') {
+        inputElevation.closest('.form__row').classList.add('form__row--hidden');
+        inputCadence.closest('.form__row').classList.remove('form__row--hidden');
+    }
+});
