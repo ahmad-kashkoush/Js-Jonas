2fe9dc091fc6392c85ec166ab834b09f839d7216
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-10-08 18:32:25 +0300

    Azan Function

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -2,6 +2,7 @@
 'use strict';
 
 // Promisify geolocation
+
 const dateFormat = function (date) {
   const dte = new Date(date);
   return `${dte.getDate()}, ${dte.getMonth() + 1}, ${dte.getFullYear()}`
@@ -12,42 +13,53 @@ const checkDate = function (a, b) {
   return aa === bb;
 }
 const getJson = function (url, err = "something went wrong") {
-  return fetch(url)
-    .then((data) => {
-      console.log(data);
-      if (data.status === 403) {
-        throw (`${err},you're loading too fast (${data.status})`);
-      }
-      if (data.status === 404) {
-        throw (`${err}, not found(fuck you)`);
-      }
-      return data.json()
-    });
+  return new Promise(function (resolve, reject) {
+    fetch(url)
+      .then((data) => {
+        if (data.status === 403) {
+          reject(`${err},you're loading too fast (${data.status})`);
+        }
+        if (data.status === 404) {
+          reject(`${err}, not found(fuck you)`);
+        }
+        resolve(data.json());
+      });
+  })
 }
+
+
 const getCurrentPosition = function () {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   })
 }
-getCurrentPosition().then(loc => {
-  console.log(loc);
-  const date = new Date();
-  const curDate = `${date.getFullYear()}/${date.getMonth() + 1}`;
-  const [lat, lng] = [loc.coords.latitude, loc.coords.longitude];
+//////////////////////////////////
+const getAzan = function () {
+  getCurrentPosition()
+    .then((loc) => {
+      // get time and place
+      const date = new Date();
+      const curDate = `${date.getFullYear()}/${date.getMonth() + 1}`;
+      const { latitude: lat, longitude: lng } = loc.coords;
 
 
-  return getJson(`http://api.aladhan.com/v1/calendar/${curDate}?latitude=${lat}&longitude=${lng}&method=5`)
-    .then(response => {
-      response.data.forEach(ele => {
-        if (checkDate(ele.date.readable, new Date())) {
-          console.log(ele.timings);
-        }
-      });
-    })
+      return getJson(`http://api.aladhan.com/v1/calendar/${curDate}?latitude=${lat}&longitude=${lng}&method=5`)
+        .then(response => {
+          let [{ timings }] =
+            response.data.filter(ele => {
+              if (checkDate(ele.date.readable, new Date()))
+                return ele;
+            })
+          if (!timings)
+            throw ("not valid link");
+          return timings;
+
+        })
+        .then(ele => console.log(ele))
+        .catch(err => console.log(err))
+    });
 }
-)
-
-
+getAzan();
 // http://api.aladhan.com/v1/calendar/2017/4?latitude=51.508515&longitude=-0.1254872&method=2http://api.aladhan.com/v1/calendar/2019?latitude=51.508515&longitude=-0.1254872&method=2
 // // building promises
 // const lotteryTicket = new Promise(function (resolve, reject) {
