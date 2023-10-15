4eabe638e16fb612a4e7da40cff0b7bb52db9594
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-10-15 15:41:40 +0300

    Fixing Variable names

--- 17-Modern-JS-Modules-Tooling/clean.js
+++ 17-Modern-JS-Modules-Tooling/clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +9,43 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimit = (user => spendingLimits?.[user] ?? 0);
+const addExpense = function (value, description, user = "jonas") {
   user = user.toLowerCase();
-
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
+  if (value <= getLimit(user)) {
     budget.push({ value: -value, description: description, user: user });
   }
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+// console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+const check = function () {
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+  budget.forEach(entry => {
+    if (entry.value < -getLimit(entry.user))
+      entry.flag = 'limit';
+  })
+
+
 };
 check();
 
-console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
+const logBigExpenses = function (limit) {
+  let output = '';
+  for (const entry of budget) {
+    output += entry.value <= -limit ? entry.description.slice(-2) + ' / ' : ''; // Emojis are 2 chars
+
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+console.log(budget);
+logBigExpenses(0);
